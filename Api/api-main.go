5d4b74a0729725b1b15939a117752727e366514e
Zachary·Smith <13212341544@163.com>
2025-08-14 14:58:06 +0800

    添加Go API项目文件

---
+++ Api/api-main.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello, World!"})
+	})
+}
